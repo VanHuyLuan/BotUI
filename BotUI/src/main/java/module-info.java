0bc4f8b3edf454e260b89498c86f834666111398
VanHuyLuan <124419803+VanHuyLuan@users.noreply.github.com>
2023-12-10 19:44:34 +0700

    first

---
+++ BotUI/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.example.botui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.botui to javafx.fxml;
+    exports com.example.botui;
+}
